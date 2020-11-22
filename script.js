0ec46266747ca504ed9c8b7f4ae12f264665efce
Julio Cesar <czar.santos@outlook.com>
2020-11-21 22:52:54 -0300

    Definindo movimentos

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box,/* definindo tamanho */
 }
+let direction = "right";/* variavel com direção, poderia ser qualquer outro */
 
 
 function criarBG(){/* background do canvas */
@@ -20,5 +21,37 @@ function criarCobrinha(){
     }
  
 }
-criarBG();
-criarCobrinha();
+
+/* função que atualiza o jogo em time, e parar o jogo quando cobra bater */
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();/* chamando outras funções */
+
+    /* Ponto de partida */
+    let snakeX = snake[0].x;/* array na posição 0 x  */
+    let snakex = snake[0].y;/* array na posição 0 Y */
+
+    /* Cordenadas */
+    if(direction == "right") snakeX += box; /* se posição for igual a direita acrescentar quadrado a mais */
+    if(direction == "left") snakeX -= box; /* Esquerda | Pra direita aumenta e pra esquerda diminui */
+    if(direction == "up") snakeY -= box;/* Y Pra cima */
+    if(direction == "down") snakeY += box;/* Y Pra baixo */
+
+    /* Retira ultimo elemento do Array */
+    snake.pop();
+
+    /* Acrescenta +1 a no primeiro elemento */
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); /* passanndo intervalo para função iniciarJogo atualizar */
+
+
+
+
+
